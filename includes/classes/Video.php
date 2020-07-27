43d70f60dba5f2a1268c338e0c4683941770a5b8
Mirko Skopljak <skopljak.mirko93@gmail.com>
2020-07-27 14:01:03 +0200

    Creating the watch video page

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -12,7 +12,7 @@ class Video {
         if (is_array($input)) {
             $this->sqlData = $input;
         } else {
-            $query = $this->con->prepare("SELECT * FROM entities WHERE id = :id");
+            $query = $this->con->prepare("SELECT * FROM videos WHERE id = :id");
             $query->bindValue(":id", $input);
             $query->execute();
 
@@ -50,5 +50,12 @@ class Video {
     public function getEpisodeNumber()
     {
         return $this->sqlData["episode"];
+    }
+
+    public function incrementViews()
+    {
+        $query = $this->con->prepare("UPDATE videos SET views = views + 1 WHERE id = :id");
+        $query->bindValue(":id", $this->getId());
+        $query->execute();
     }
 }
--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
 
     <link rel="stylesheet" href="assets/css/style.css">
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/96beaf737f.js" crossorigin="anonymous"></script>
     <script src="assets/js/script.js"></script>
 </head>
